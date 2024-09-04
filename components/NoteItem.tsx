bedd26c629484c3b69cbcd993c3c28b6921c1bf0
Gerardo Medellin <out_jmedellin@findep.com.mx>
2024-09-03 20:03:39 -0600

    update: added tag identification of locked notes at the end of the note item card

--- components/NoteItem.tsx
+++ components/NoteItem.tsx
@@ -9,9 +9,14 @@ export default function NoteItem({note}: NoteItemProps) {
   return (
     <Pressable style={({pressed}) => pressed && styles.pressed} onPress={() => console.log(`noteitem: ${note.id}`)}>
         <View style={styles.card}>
-            <Text style={styles.noteTitle}>{note.title}</Text>
-            <Text style={styles.noteDescription}>{limitContent(note.content, 40)}</Text>
-            <Text style={styles.noteDate}>Last modified: {note.date}</Text>
+            <View style={styles.textContainer}>
+                <Text style={styles.noteTitle}>{note.title}</Text>
+                <Text style={styles.noteDescription}>{limitContent(note.content, 40)}</Text>
+                <Text style={styles.noteDate}>Last modified: {note.date}</Text>
+            </View>
+            <View style={styles.tagContainer}>
+                {note.locked === 1 && <Text style={styles.privateTag}>Private</Text>}
+            </View>
         </View>
     </Pressable>
   )
@@ -22,12 +27,18 @@ const styles = StyleSheet.create({
         opacity: 0.5
     },
     card: {
-        backgroundColor: 'gray',
+        backgroundColor: '#212121',
         padding: 10,
         margin: 10,
         borderRadius: 10,
         width: '90%',
-        alignSelf: 'center'
+        alignSelf: 'center',
+        flexDirection: 'row', // Add this line
+        justifyContent: 'space-between', // Add this line
+        alignItems: 'center' // Add this line
+    },
+    textContainer: {
+        flex: 1,
     },
     noteTitle: {
         color: 'white',
@@ -41,5 +52,18 @@ const styles = StyleSheet.create({
     noteDate: {
         color: 'white',
         opacity: 0.5
-    }
+    },
+    tagContainer: {
+        justifyContent: 'center',
+        alignItems: 'flex-end',
+    },
+    privateTag: {
+        color: 'green',
+        fontWeight: 'bold',
+        borderColor: 'green',
+        borderWidth: 1,
+        padding: 5,
+        borderRadius: 6,
+        fontSize: 10
+    },
 });
